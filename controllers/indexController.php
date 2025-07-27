36210a9cc90cf39546ad266e216317ac43fa4aa9
Ivanjij <ivanxaldairxrc@gmail.com>
2025-07-27 00:39:25 -0600

    Update

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -5,7 +5,7 @@ class indexController
     // Método para renderizar el navbar
     public function renderNavbar()
     {
-        include_once '/includes/navbar.php';
+        include_once 'includes/navbar.php';
     }
 
     // Método para renderizar el initiation
