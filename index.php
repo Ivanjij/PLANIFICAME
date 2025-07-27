4c19ccdba8c8943a0d25ed346468715babe735c5
Ivanjij <ivanxaldairxrc@gmail.com>
2025-07-26 21:48:51 -0600

    Update

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
 // Incluir el controlador principal
 include_once 'controllers/indexController.php';
-?>
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<link rel="stylesheet" href="css/index.css">
-
-<?php
 // Crear una instancia del controlador
 $controller = new indexController();
 $controller->renderNavbar();  // Método para renderizar el navbar
 $controller->renderInitiation();  // Método para renderizar el Initiation
 ?>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="css/index.css">
