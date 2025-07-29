813f4c3c45cec8261981078b8f5279fcc31269c2
Ivanjij <ivanxaldairxrc@gmail.com>
2025-07-29 00:41:43 -0600

    ss

--- views/login/register.php
+++ views/login/register.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="/css/register.css" />
+<link rel="stylesheet" href="/var/www/html/css/register.css" />
 <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
 
 <canvas id="particle-canvas"></canvas>
